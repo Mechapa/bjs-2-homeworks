862d0ea68b59b7206fadcb492c6ed34368949ba8
Mechapa <mechapamachito@gmail.com>
2022-05-24 18:25:39 +0300

    first commit 2nd exersise

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,53 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
-
-  // Ваш код
-
-  return { min: min, max: max, avg: avg };
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0;
+  let avg = 0;
+    for (let i = 0; i < arr.length; i++) {
+      sum = sum + arr[i];
+        if (arr[i] > max) {
+          max = arr[i];
+        }
+        if (arr[i] < min) {
+          min = arr[i];
+        }
+    }
+  avg = parseFloat((sum / arr.length).toFixed(2));
+  return { min:min, max:max, avg:avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
+  let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+      sum = sum + arr[i];
+    }
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
-
-  // Ваш кода
-  // for ...
-  
+  let max = -Infinity;
+    for (let i = 0; i < arrOfArr.length; i++) {
+      let arrCheck = func(arrOfArr[i]);
+      if (arrCheck > max) {
+        max = arrCheck;
+      }
+    }
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let min = Infinity;
+  let max = -Infinity;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] > max) {
+        max = arr[i];
+      }
+      if (arr[i] < min) {
+        min = arr[i];
+      }
+    }
+  return Math.abs(max-min);
 }
