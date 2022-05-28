f5ce92d53b79dcbe9b9476e5f4606a6a53d20f4f
Mechapa <mechapamachito@gmail.com>
2022-05-28 23:50:16 +0300

    first commit third task

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,12 @@
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
+  result = (arr1.length === arr2.length) && (arr1.every((element, i) => element === arr2[i]));
 
   return result; // boolean
 }
 
 function advancedFilter(arr) {
   let resultArr;
-
-  // Ваш код
-
+  resultArr = arr.filter(item => item > 0).filter(item => item % 3 === 0).map(item => item * 10);
   return resultArr; // array
 }
