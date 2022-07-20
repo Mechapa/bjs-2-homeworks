9965c5b35d3458b07dbfc681829e5edf7797ca0d
Mechapa <mechapamachito@gmail.com>
2022-07-20 21:26:47 +0300

    third commit eighth task

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -2,11 +2,11 @@ function cachingDecoratorNew(func) {
   let cache = [];
   function wrapper(...args) {
     const hash = args.join(",");
-    let objectInCache = cache.findIndex((item) => item.hash == hash);
-    if (objectInCache > (-1)) {
-      console.log("Их кэша: " + cache[objectInCache].result);
-      return "Из кэша: " + cache[objectInCache].result;
-    } else {
+    let objectInCache = cache.find((item) => item.hash == hash);
+    if (objectInCache != undefined) {
+      console.log("Их кэша: " + objectInCache.result);
+      return "Из кэша: " + objectInCache.result;
+    }
       let result = func.call(this, ...args);
       cache.push({hash, result});
       if (cache.length > 4) {
@@ -14,7 +14,6 @@ function cachingDecoratorNew(func) {
       }
       console.log("Вычисляем: " + result);
       return "Вычисляем: " + result;  
-    }
   }
   return wrapper;
 }
